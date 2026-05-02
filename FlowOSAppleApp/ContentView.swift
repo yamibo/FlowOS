3666aad3e7b4e723dde2b57e536fc8cc34ec5d8b
Yamibo <Celsius17@gmail.com>
2026-05-02 20:49:21 +0800

    Initial Commit

---
+++ FlowOSAppleApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FlowOSAppleApp
+//
+//  Created by Celsius on 2026/5/2.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FlowOSAppleApp/FlowOSAppleAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlowOSAppleAppApp.swift
+//  FlowOSAppleApp
+//
+//  Created by Celsius on 2026/5/2.
+//
+
+import SwiftUI
+
+@main
+struct FlowOSAppleAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
